49873dd84d501c12c4b56335bd3ab2903d6bcd5c
YGOPRODeck <37552458+AlanOC91@users.noreply.github.com>
2023-02-23 11:32:39 +0000

    Add OpenGraph Image and Meta Description

--- app/head.js
+++ app/head.js
@@ -1,10 +1,14 @@
 export default function Head() {
-  return (
-    <>
-      <title>YGOSearch - Instant Yu-Gi-Oh! Card Search</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="YGOSearch - Instant Yu-Gi-Oh! Card Search" content="An instant Yu-Gi-Oh! Card Search App from YGOPRODeck!" />
-      <link rel="icon" href="/favicon.ico" />
-    </>
-  )
+    return (
+        <>
+            <title>YGOSearch - Instant Yu-Gi-Oh! Card Search</title>
+            <meta content="width=device-width, initial-scale=1" name="viewport"/>
+            <meta name="YGOSearch - Instant Yu-Gi-Oh! Card Search"
+                  content="An instant Yu-Gi-Oh! Card Search App from YGOPRODeck!"/>
+            <meta name="description" content="An instant Yu-Gi-Oh! Card Search App from YGOPRODeck!"/>
+            <link rel="icon" href="/favicon.ico"/>
+            <meta property="og:image" content="https://www.ygosearch.io/ygoprodeck.png"/>
+            <meta property="og:image:secure_url" content="https://www.ygosearch.io/ygoprodeck.png"/>
+        </>
+    )
 }
